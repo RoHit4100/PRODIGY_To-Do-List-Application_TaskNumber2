20ad45ad3376782ede1a308ce32c5d16421b6eff
RoHit4100 <rohitudamale777@gmail.com>
2022-12-23 20:02:42 +0530

    Done With the Functionality and styling of the project

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 let tasks = ["Wake in the morning", "Web development", "Data structures"];
-
-
+let workList = [];
 const app = express();
 app.set("view-engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
+
     let date = new Date();
     let options = {
         weekday: "long",
         day: "numeric",
         month: "short"
     };
+    let pageTitle = date.toLocaleDateString("en-us", options);
+    res.render("list.ejs", { heading: pageTitle, newTasks: tasks });
 
-    let formatDay = date.toLocaleDateString("en-us", options);
-    res.render("list.ejs", { kindOfDay: formatDay, newTasks: tasks });
+
 });
+
+app.get("/work", function (req, res) {
+    console.log(req.body);
+    res.render("list.ejs", { heading: "Work List", newTasks: workList })
+})
 
 app.post("/", function (req, res) {
     let task = req.body.nextTask;
-    tasks.push(task);
-
-    res.redirect("/");
+    if (req.body.List === "Work") {
+        workList.push(task)
+        res.redirect("/work")
+    } else {
+        console.log(task);
+        tasks.push(task)
+        res.redirect("/")
+    }
 })
+
+app.post("/work", function (req, res) {
+    let task = req.body.nextTask;
+    workList.push(workItem);
+    res.redirect("/work");
+})
+
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
