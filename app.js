1382e51c7bcabb228941682f4aac1643ef1c1a1c
RoHit4100 <rohitudamale777@gmail.com>
2022-12-23 17:20:22 +0530

    Functionality is added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+let tasks = ["Wake in the morning", "Web development", "Data structures"];
+
 
 const app = express();
 app.set("view-engine", "ejs");
@@ -7,15 +9,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     let date = new Date();
-    let today = date.getDate();
-    let day = "";
-    if (today === 0 || today === 6) {
-        day = "Weekend";
-    } else {
-        day = "weekday"
-    }
-    res.render("list.ejs", { kindOfDay: day });
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "short"
+    };
+
+    let formatDay = date.toLocaleDateString("en-us", options);
+    res.render("list.ejs", { kindOfDay: formatDay, newTasks: tasks });
 });
+
+app.post("/", function (req, res) {
+    let task = req.body.nextTask;
+    tasks.push(task);
+
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
